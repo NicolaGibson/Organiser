4269d9df92c1d10f60a0a758217b7b821491d61e
Nicola Gibson <nicola.k.gibson@gmail.com>
2019-04-05 11:13:19 +0100

    basic project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class App extends React.Component{
+  constructor()
+  props(){
+
+    }
+componentDidMount(){
+    
+}
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    
+    }
+  
+
+}
